9f54e497847abf807916975097cae9bfa4bffc23
EricWithC04 <negativegame117@gmail.com>
2024-03-20 20:46:53 -0300

    server with addition function

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 import express from "express"
 const app = express()
 
-app.get("/matrixAddition", (req, res) => {
-    res.status(200).send("Hello World!")
+app.use(express.json())
+
+app.post("/matrixAddition", (req, res) => {
+
+    const matrix1 = req.body.matrix1
+    const matrix2 = req.body.matrix2
+    const newMatrix = []
+
+    for (let i = 0; i < matrix1.length; i++) {
+        newMatrix.push([])
+        for (let j = 0; j < matrix1[i].length; j++) {
+            newMatrix[i].push(matrix1[i][j] + matrix2[i][j])
+        }
+    }
+
+    res.status(200).json(newMatrix)
 })
 
 app.listen(3000, () => {
